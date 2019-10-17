5b5d4463cc87437bd03396cf0a90a16e0eb82e89
Ahmed Saber <sander.sader@gmail.com>
2019-10-17 17:00:41 +0200

    Rename MainActivity to PhotosListActivity and move it to the feature specific location.

---
+++ app/src/main/java/com/example/flickrdemo/features/photos/presentation/PhotosListActivity.kt
@@ -0,0 +1,13 @@
+package com.example.flickrdemo.features.photos.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.flickrdemo.R
+
+class PhotosListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photos_list)
+    }
+}
