01a74d315ba462f34b4c8eb8204fdf1d0b2e51ec
Ahmed Saber <sander.sader@gmail.com>
2019-10-17 17:24:48 +0200

    Add photo details service api end point.

--- app/src/main/java/com/example/flickrdemo/utilities/Constants.kt
+++ app/src/main/java/com/example/flickrdemo/utilities/Constants.kt
@@ -5,6 +5,8 @@ class Constants {
         const val API_KEY = "5112719369700f441b306c1800a21524"
         const val API_METHOD_NAME = "flickr.photos.getRecent"
         const val LIST_OF_PHOTOS_SERVICE_END_POINT = "https://api.flickr.com/services/rest/"
+        const val PHOTO_SERVICE_END_POINT =
+            "https://farm{farmId}.staticflickr.com/{serverId}/{secretId}_{photoId}.jpg"
         const val CATEGORIES_PAGE_LIMIT = 10
     }
 }
