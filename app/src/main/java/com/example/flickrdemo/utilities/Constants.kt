a824de855638749f9148c74ec248b90740c1afd9
Ahmed Saber <sander.sader@gmail.com>
2019-10-17 16:40:44 +0200

    Add Constants file; Required flickr api data.

---
+++ app/src/main/java/com/example/flickrdemo/utilities/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.flickrdemo.utilities
+
+class Constants {
+    companion object {
+        const val API_KEY = "5112719369700f441b306c1800a21524"
+        const val GALLERY_ID = "66911286-72157647277042064"
+        const val API_METHOD_NAME = "flickr.galleries.getPhotos"
+        const val LIST_OF_PHOTOS_SERVICE_END_POINT = "https://api.flickr.com/services/rest/"
+        const val CATEGORIES_PAGE_LIMIT = 10
+    }
+}
