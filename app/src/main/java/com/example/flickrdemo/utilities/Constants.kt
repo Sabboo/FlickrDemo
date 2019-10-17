0d83e59313539c7b5fbc364f68b155a11e215fca
Ahmed Saber <sander.sader@gmail.com>
2019-10-17 20:32:02 +0200

    Constants updates. Renaming Photos services end point name.

--- app/src/main/java/com/example/flickrdemo/utilities/Constants.kt
+++ app/src/main/java/com/example/flickrdemo/utilities/Constants.kt
@@ -4,7 +4,7 @@ class Constants {
     companion object {
         const val API_KEY = "5112719369700f441b306c1800a21524"
         const val API_METHOD_NAME = "flickr.photos.getRecent"
-        const val LIST_OF_PHOTOS_SERVICE_END_POINT = "https://api.flickr.com/services/rest/"
+        const val FLICKR_SERVICE_BASE_URL = "https://api.flickr.com/services/"
         const val PHOTO_SERVICE_END_POINT =
             "https://farm{farmId}.staticflickr.com/{serverId}/{secretId}_{photoId}.jpg"
         const val PHOTOS_PAGE_LIMIT = 10
