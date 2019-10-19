a1a4be5f42e3c3bda5bfeda6d33bc25b039d9b29
Ahmed Saber <sander.sader@gmail.com>
2019-10-19 16:37:44 +0200

    Implementing Parcelable interface for Photo model.

--- app/src/main/java/com/example/flickrdemo/features/photos/data/model/Photo.kt
+++ app/src/main/java/com/example/flickrdemo/features/photos/data/model/Photo.kt
@@ -1,13 +1,16 @@
 package com.example.flickrdemo.features.photos.data.model
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class Photo(
     @SerializedName("id") val id: String,
     @SerializedName("secret") val secret: String,
     @SerializedName("server") val server: String,
     @SerializedName("farm") val farm: String
-) {
+) : Parcelable {
     fun photoURL() = String.format(
         "https://farm%s.staticflickr.com/%s/%s_%s.jpg",
         farm, server, id, secret
